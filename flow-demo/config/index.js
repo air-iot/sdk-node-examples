931885491898ba2e0ffef0b601db453dce583696
zhangqiang <z397435052@gmail.com>
2023-08-29 15:14:13 +0800

    fix: 配置rc调整

--- flow-demo/config/index.js
+++ flow-demo/config/index.js
@@ -3,7 +3,7 @@ let rc = require('rc')
 // 因为 rc 是从 process.cwd() 向上查找 .appnamerc 文件的，我们在根目录 config 文件夹里面的是找不到的，要改变工作路径到当前，再改回去
 var originCwd = process.cwd()
 process.chdir(__dirname)
-var conf = rc('dev', {
+var conf = rc('win', {
   flow: {
     name: "flowTestNode",
     mode: "service"
