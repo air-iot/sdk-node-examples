cb1226e99f21ec5e7c5b03e8f0ba7855705aaca8
zhangqiang <z397435052@gmail.com>
2023-09-07 10:31:26 +0800

    fix: 流程扩展节点

--- flow-extension-demo/schema.js
+++ flow-extension-demo/schema.js
@@ -1,29 +1,17 @@
 {
-  "title": "加法",
-  "input": {
-    "type": "object",
-    "properties": {
-      "num1": {
-        "title": "参数1",
-        "type": "number"
-      },
-      "num2": {
-        "title": "参数2",
-        "type": "number"
-      }
-    },
-    "required": [
-      "num1",
-      "num2"
-    ]
+  "type": "object",
+  "properties": {
+  "num1": {
+    "title": "参数1",
+      "type": "number"
   },
-  "output": {
-    "type": "object",
-    "properties": {
-      "num1": {
-        "title": "结果",
-        "type": "number"
-      }
-    }
+  "num2": {
+    "title": "参数2",
+      "type": "number"
   }
+},
+  "required": [
+  "num1",
+  "num2"
+]
 }
